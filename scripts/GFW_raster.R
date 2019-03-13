20094f01e0adfb17bfd36b9b5af5109407409e97
Juan Carlos <juancarlos.villader@gmail.com>
2019-03-13 21:18:08 +0000

    Changed the name to reflect that we now use 3.4 index

--- scripts/GFW_raster.R
+++ scripts/GFW_raster.R
@@ -20,18 +20,15 @@ world_coastline <- rnaturalearth::ne_countries(returnclass = "sf")
 world_coastline2 <- rnaturalearth::ne_coastline(returnclass = "sf")
 
 # Read the fishing effort
-gridded_ff <- readRDS(file = here::here("raw_data", "gridded_ff_by_gear_country.rds")) %>% 
-  filter(best_label %in% c("drifting_longlines", "set_longlines", "drifting_longlines|set_longlines", "tuna_purse_seines", "other_purse_seines", "purse_seines")) %>% 
-  mutate(date = date(paste(year, month, "01", sep = "/")),
-         best_label = ifelse(str_detect(best_label, "purse"), "purse_seines", best_label))
+gridded_ff <- readRDS(file = here::here("raw_data", "gridded_ff_by_gear_country.rds"))
 
 # Plot the raster
 p <- gridded_ff %>% 
   filter(hours > 0) %>% 
-  group_by(is_foreign, latitude, longitude) %>% 
+  group_by(foreign, latitude, longitude) %>% 
   summarize(hours = sum(hours, na.rm = T)) %>% 
   ungroup() %>% 
-  mutate(is_foreign = ifelse(is_foreign, "Foreign", "Local")) %>% 
+  mutate(foreign = ifelse(foreign, "Foreign", "Local")) %>% 
   ggplot() +
   ggtheme_map() +
   theme_dark() +
