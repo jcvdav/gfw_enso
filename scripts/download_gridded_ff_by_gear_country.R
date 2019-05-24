3d8475d63f31b688f2ad079e54eab9b52fbbafcb
jcvdav <juancarlos.villader@gmail.com>
2019-05-24 15:47:12 -0700

    Added script for eez-level correlations to run on the server

--- scripts/download_gridded_ff_by_gear_country.R
+++ scripts/download_gridded_ff_by_gear_country.R
@@ -5,18 +5,18 @@
 # Load packages
 library(startR)
 library(lubridate)
+library(here)
 library(tidyverse)
 
 # Create a tbl and collect it in gff in case anything fails later
 gff <- get_table(dataset = "enso_gfw",
-                 table = "gridded_ff_by_gear_country",
-                 show_tables = T)
+                 table = "gridded_ff_by_gear_country")
 
 # If the above didn't fail, we now tidy-up our data
 gff <- gff %>%
   distinct() %>% 
   mutate(foreign = as.logical(foreign)) %>% 
-  mutate(date = lubridate::date(paste(year, month, "01", sep = "/"))) %>% 
+  mutate(date = date(paste(year, month, "01", sep = "/"))) %>% 
   select(year,
          month,
          date,
@@ -29,4 +29,5 @@ gff <- gff %>%
          everything())
 
 # Export the data as rds to keep the present characteristics
-saveRDS(gff, file = here::here("raw_data", "gridded_ff_by_gear_country.rds"))
+saveRDS(object = gff,
+        file = here("raw_data", "gridded_ff_by_gear_country.rds"))
