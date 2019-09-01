a8ef9b0a205aefdf524b9fdd9e556794ffd251f4
jcvdav <juancarlos.villader@gmail.com>
2019-08-31 17:53:27 -0700

    Changed resolution and added code for the seiners

--- scripts/processing/4_fishing_effort_rasters.R
+++ scripts/processing/4_fishing_effort_rasters.R
@@ -10,14 +10,26 @@ library(tidyverse)
 # Define projection
 proj <- "+proj=longlat +datum=WGS84 +no_defs"
 
-# Download data from GBQ
-# gridded_effort <- get_table(dataset = "enso_gfw",
-#                             table = "gfw_enso_gridded_monthly_effort_by_gear")
 
-gridded_effort <- readRDS(here("data", "gridded_effort.rds"))
+
+# Get the data
+
+
+if(file.exists(here("data", "gridded_monthly_effort_by_gear.rds"))){
+  # Download data from GBQ
+  gridded_effort <- get_table(dataset = "enso_gfw",
+                              table = "gfw_enso_gridded_monthly_effort_by_gear")
+  
+  # Export the data
+  saveRDS(object = gridded_effort,
+          file = here("data", "gridded_effort.rds"))
+} else {
+  # If the file exists, read it instead
+  gridded_effort <- readRDS(here("data", "gridded_effort.rds"))
+}
 
 # Function to rasterize each month
-my_rasterize <- function(x, res = 0.01, proj = "+proj=longlat +datum=WGS84 +no_defs") {
+my_rasterize <- function(x, res = 0.1, proj = "+proj=longlat +datum=WGS84 +no_defs") {
   # Create raster
   r <- rasterFromXYZ(x, res = res, crs = proj)
   
@@ -32,9 +44,19 @@ my_rasterize <- function(x, res = 0.01, proj = "+proj=longlat +datum=WGS84 +no_d
 # Set up parallelization
 plan(multiprocess)
 
+# Trawlers
 gridded_effort_trawlers <- gridded_effort %>% 
   filter(best_vessel_class == "trawlers",
-         year == 2018) %>% 
+         year < 2018) %>% 
+  select(year, month, x = lon_bin_center, y = lat_bin_center, hours) %>% 
+  group_by(year, month) %>% 
+  nest() %>%
+  mutate(r = future_map(data, my_rasterize))
+
+# Seiners
+gridded_effort_seiners <- gridded_effort %>% 
+  filter(!best_vessel_class == "trawlers",
+         year < 2018) %>% 
   select(year, month, x = lon_bin_center, y = lat_bin_center, hours) %>% 
   group_by(year, month) %>% 
   nest() %>%
@@ -43,27 +65,16 @@ gridded_effort_trawlers <- gridded_effort %>%
 # "Close" background workers
 plan(sequential)
 
-trawlers <- stack(gridded_effort_trawlers$r)
+# Add names to each layer before exporting
 
-writeRaster(trawlers, filename = here("data", "trawlers.nc"))
-
-# gridded_effort_seiners <- gridded_effort %>% 
-#   filter(!best_vessel_class == "trawlers") %>% 
-#   select(year, month, x = lon_bin_center, y = lat_bin_center, hours) %>% 
-#   group_by(year, month) %>% 
-#   nest() %>%
-#   mutate(r = map(data, my_rasterize))
+trawlers <- brick(gridded_effort_trawlers$r)
+saveRDS(trawlers,
+        filename = here("data", "trawlers.rds"))
 
 
-# bench <- microbenchmark::microbenchmark(
-#   rasterize = r <- st_as_sf(gridded_effort_trawlers, coords = c("x", "y"), crs = proj) %>% 
-#     rasterize(x = .,y = ra, field = "hours"),
-#   fasterize = f <- rasterFromXYZ(gridded_effort_trawlers, res = 0.01, crs = proj),
-#   unit = "ms"
-# )
-# 
-# print(bench, digits = 3)
-
+seiners <- brick(gridded_effort_seiners$r)
+saveRDS(seiners,
+        filename = here("data", "seiners.rds"))
 
 
 
