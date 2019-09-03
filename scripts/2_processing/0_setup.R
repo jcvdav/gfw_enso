a3d5dff0e99e123b23deaccc9dee51b85c22a405
jcvdav <juancarlos.villader@gmail.com>
2019-09-03 10:51:12 -0700

    Added raster optionas and temp file deletion function to setup script

--- scripts/2_processing/0_setup.R
+++ scripts/2_processing/0_setup.R
@@ -8,3 +8,19 @@ proj_beh <- "+proj=cea +lon_0=0 +lat_ts=30 +x_0=0 +y_0=0 +datum=WGS84 +ellps=WGS
 
 # Geometry precisions
 geometry_precision <- 500
+
+# Temoporary raster locations
+raster::rasterOptions(tmpdir = here::here("data", "raster_tmp"),
+                      overwrite = T,
+                      maxmemory = 12e+09,
+                      memfrac = 0.8)
+
+# Function to remove temp files from disk
+rm_raster_tmp <- function(){
+  # Get the files
+  raster_tmp_files <- list.files(path = here::here("data", "raster_tmp"),
+                                 pattern = "r_tmp")
+  
+  # Remove them
+  file.remove(here("data", "raster_tmp", raster_tmp_files)) 
+}
