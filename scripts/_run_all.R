1f7b645394f81806c5301eab20212a85c81289b9
jcvdav <juancarlos.villader@gmail.com>
2019-09-03 10:52:25 -0700

    Updated the run_all script

--- scripts/_run_all.R
+++ scripts/_run_all.R
@@ -1,15 +1,22 @@
 
-library(here)
+if(!require(here)){
+  install.packages("here")
+  library(here)
+}
+
+#### SET UP ####
+# Install packages
+source("1_setup.R")
 
 #### PROCESSING ####
 # Clean WDPA polygons and rasterize them
-source(here("scripts", "processing", "1_clean_wdpa.R"))
+source(here("scripts", "2_processing", "1_clean_wdpa.R"))
 # Estimate correlation between ENSO and SST at 2 degree resolution
-source(here("scripts", "processing", "2_cor_sst_nino34_2deg.R"))
+source(here("scripts", "2_processing", "2_cor_sst_nino34_2deg.R"))
 # Identify teleconnected regions and create raster
-source(here("scripts", "processing", "3_identify_teleconnected_regions.R"))
+source(here("scripts", "2_processing", "3_identify_teleconnected_regions.R"))
 # Create a fishing effort raster
-source(here("scripts", "processing", "4_fishing_effort_rasters.R"))
+source(here("scripts", "2_processing", "4_fishing_effort_rasters.R"))
 
 
 #### Analyses ####
