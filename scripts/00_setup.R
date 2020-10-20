7c05b4655c9fd81727969963593cda8c6d00eb5a
Juan Carlos Villasenor Derbez <juancarlos@ucsb.edu>
2020-10-20 09:46:21 -0700

    Cleaning repo

---
+++ scripts/00_setup.R
@@ -0,0 +1,12 @@
+##########################
+## Paths to directories ##
+##########################
+
+# Check for OS
+sys_path <- startR::get_filestream_path()
+
+# Path to emLab data folder
+data_path <- file.path(sys_path,"Shared drives/emlab/data")
+
+# Path to this project folder
+project_path <- file.path(sys_path,"Shared drives/emlab/projects/current-projects/gfw-enso")
