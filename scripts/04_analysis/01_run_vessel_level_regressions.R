0871408f632b47c18176fe621e24eeffef40efc5
Juan Carlos Villasenor Derbez <juancarlos@ucsb.edu>
2020-11-06 09:32:23 -0800

    Updating results with Kyle's input

--- scripts/04_analysis/01_run_vessel_level_regressions.R
+++ scripts/04_analysis/01_run_vessel_level_regressions.R
@@ -24,17 +24,15 @@ indep_vars <- c("nino34anom | ssvid + month",
 my_lm <- function(formula, data){
   formula <- as.formula(formula)
   feols(formula, data) %>% 
-    summary(cluster = c("ssvid", "month"))
+    summary(cluster = c("ssvid"))
 }
 
 
 ps_panel <- panel %>% 
-  filter(gear == "tuna_purse_seines") %>% 
-  drop_na(nino34anom)
+  filter(gear == "tuna_purse_seines")
 
 ll_panel <- panel %>% 
-  filter(gear == "drifting_longlines") %>% 
-  drop_na(nino34anom)
+  filter(gear == "drifting_longlines")
 
 ps_results <- expand_grid(dep_vars, indep_vars) %>% 
   mutate(formula = paste(dep_vars, indep_vars, sep = " ~ ")) %>% 
@@ -45,6 +43,17 @@ ll_results <- expand_grid(dep_vars, indep_vars) %>%
   mutate(model = future_map(formula, my_lm, data = ll_panel))
 
 make_table <- function(models, dep_var, title, filename) {
+  
+  dep_var1 <- dep_var2 <- dep_var
+  dep_var2 <- paste("Share", tolower(dep_var2))
+  
+
+  if(length(dep_var) > 1){
+    dep_var1 <- dep_var[1]
+    dep_var2 <- dep_var[2]
+  }
+  
+
   models %>% 
     modelsummary(title = title,
                  coef_omit = "year|month|ssvid|flag",
@@ -53,8 +62,8 @@ make_table <- function(models, dep_var, title, filename) {
                  output = "gt",
                  gof_omit = "Adj|Pse|With|AIC|BIC|Log",
                  coef_map = c("(Intercept)" = "Constant", "nino34anom" = "nino34anom")) %>% 
-    tab_spanner(label = dep_var, columns = 2:4) %>%
-    tab_spanner(label = paste("Share", tolower(dep_var)), columns = 5:7) %>%
+    tab_spanner(label = dep_var1, columns = 2:4) %>%
+    tab_spanner(label = dep_var2, columns = 5:7) %>%
     tab_style(style = cell_text(color = 'red'),
               locations = cells_body(rows = 1)) %>% 
     gtsave(filename = here::here("results", "tab", filename))
@@ -65,7 +74,7 @@ ps_results %>%
   filter(!str_detect(dep_vars, "kilo")) %>% 
   pull(model) %>% 
   make_table(dep_var = "Foreign fishing hours",
-             title = "Purse Seines",
+             title = "Purse Seines hours",
              filename = "vessel_level_purse_seines_regressions_hours.html")
 
 # Purse seines energy table
@@ -73,7 +82,7 @@ ps_results %>%
   filter(str_detect(dep_vars, "kilo")) %>% 
   pull(model) %>% 
   make_table(dep_var = "Foreign fishing kilowatthours",
-             title = "Purse Seines",
+             title = "Purse Seines Kilowatt-hours",
              filename = "vessel_level_purse_seines_regressions_kilowatthours.html")
 
 
@@ -84,7 +93,7 @@ ll_results %>%
   filter(!str_detect(dep_vars, "kilo")) %>% 
   pull(model) %>% 
   make_table(dep_var = "Foreign fishing hours",
-             title = "Longlines",
+             title = "Longlines hours",
              filename = "vessel_level_longlines_regressions_hours.html")
 
 
@@ -94,6 +103,48 @@ ll_results %>%
   filter(str_detect(dep_vars, "kilo")) %>% 
   pull(model) %>% 
   make_table(dep_var = "Foreign fishing kilowatthours",
-             title = "Longlines",
+             title = "Longlines Kilowatt-hours",
              filename = "vessel_level_longlines_regressions_kilowatthours.html")
+
+
+########### TOTAL FISHING HOURS
+
+ps_totals <- expand_grid(extra_dep_vars, indep_vars) %>% 
+  mutate(formula = paste(dep_vars, indep_vars, sep = " ~ ")) %>% 
+  mutate(model = purrr::map(formula, my_lm, data = ps_panel))
+
+ll_totals <- expand_grid(extra_dep_vars, indep_vars) %>% 
+  mutate(formula = paste(dep_vars, indep_vars, sep = " ~ ")) %>% 
+  mutate(model = purrr::map(formula, my_lm, data = ll_panel))
+  
+
+## Make tables
+ps_totals %>% 
+  pull(model) %>% 
+  make_table(dep_var = c("Hours", "Kilowatt-hours"),
+             title = "Purse Seine total hour and kWh",
+             "vessel_level_purse_seines_regressions_totals.html")
+
+ll_totals %>% 
+  pull(model) %>% 
+  make_table(dep_var = c("Hours", "Kilowatt-hours"),
+             title = "Longline total hour and kWh",
+             "vessel_level_longline_regressions_totals.html")
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
